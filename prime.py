917b9930db9f88fe214e3dc05f67d5f44abdc2ab
deisler134 <microgalaxy134@gmail.com>
2019-11-11 16:26:59 -0500

    check prime

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
 def check_prime(n:int)->bool:
     if n < 1:
         return False
-    if n % 2 or n % 3 == 0:
+    if n % 2 == 0 or n % 3 == 0:
         return False
     i = 5
     while i*i <= n:
@@ -9,4 +9,9 @@ def check_prime(n:int)->bool:
             return False
         i += 6
     return True
+
+if __name__ == "__main__":
+    n = 104743
+    if check_prime(n): print('{0} is prime'.format(n))
+    else : print('{0} is not prime'.format(n))
     
